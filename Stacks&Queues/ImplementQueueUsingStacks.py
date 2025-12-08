d5a49b0eaaf1b7907f9586469916dd47dfc83c7b
sreejap <sreeja605@gmail.com>
2025-12-08 12:06:39 -0500

    Create ImplementQueueUsingStacks.py

---
+++ Stacks&Queues/ImplementQueueUsingStacks.py
@@ -0,0 +1,34 @@
+class MyQueue:
+
+    def __init__(self):
+        # use self. to attach the variables to the instance
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        # queue - first in first out
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1] # get the last element      
+
+    def empty(self) -> bool:
+        return not self.in_stack and not self.out_stack
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
