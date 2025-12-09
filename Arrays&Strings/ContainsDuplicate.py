ae8eb3f62a6830f24d281e38fdeda481447cca0e
sreejap <sreeja605@gmail.com>
2025-12-09 17:22:17 -0500

    Create ContainsDuplicate.py

---
+++ Arrays&Strings/ContainsDuplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_nums = set ()
+
+        for i in nums:
+            if i in unique_nums:
+                return True
+            else:
+                unique_nums.add(i)
+        
+        return False
