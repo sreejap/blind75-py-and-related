e4e4b047a034124bd83d1d00adab822b684e37ee
sreejap <sreeja605@gmail.com>
2026-01-12 16:14:16 -0500

    Update RangeSumQuery.py

--- Arrays&Strings/RangeSumQuery.py
+++ Arrays&Strings/RangeSumQuery.py
@@ -1,8 +1,14 @@
 from itertools import accumulate
+def init_sum_array (nums: list[int]) -> list [int]:
+    return list(accumulate(nums,initial=0))
+def range_sum_query_immutable(nums: list[int], left: int, right: int) -> int:
+    cumulative_sum = init_sum_array(nums)
+    return cumulative_sum[right+1] - cumulative_sum[left]
+ 
 
-class NumArray:
-    def __init__(self, nums: list[int]):
-        self.prefix = list(accumulate(nums, initial=0))
-
-    def sumRange(self, left: int, right: int) -> int:
-        return self.prefix[right + 1] - self.prefix[left]
+if __name__ == "__main__":
+    nums = [int(x) for x in input().split()]
+    left = int(input())
+    right = int(input())
+    res = range_sum_query_immutable(nums, left, right)
+    print(res)
