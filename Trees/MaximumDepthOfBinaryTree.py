6f98eb53e13a8969656cf8616ed31513005cdd97
sreejap <sreeja605@gmail.com>
2026-01-13 14:44:34 -0500

    Update MaximumDepthOfBinaryTree.py

--- Trees/MaximumDepthOfBinaryTree.py
+++ Trees/MaximumDepthOfBinaryTree.py
@@ -1,6 +1,12 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-        return max (self.maxDepth(root.left),self.maxDepth(root.right))+1
+def tree_max_depth(root: Node) -> int:
+    def dfs(root:Node) -> int:
+        if not root:
+            return 0
+        return max (dfs(root.left), dfs(root.right)) + 1     # we count the number of nodes in the dfs method
+    return dfs (root) - 1 if root else 0 # we subtract 1 because we want the edges in the answer as defined in the problem https://algo.monster/problems/tree_max_depth
