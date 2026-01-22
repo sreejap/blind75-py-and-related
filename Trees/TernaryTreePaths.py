dfd167e3e9a29b5691b83366aad55d9dc9efcf87
sreejap <sreeja605@gmail.com>
2026-01-22 17:02:41 -0500

    Update TernaryTreePaths.py

--- Trees/TernaryTreePaths.py
+++ Trees/TernaryTreePaths.py
@@ -5,20 +5,25 @@ class Node:
         self.val = val
         self.children = children
 
-def ternary_tree_paths(root: Node) -> list[str]:
-    res = []
-    def dfs (node,path,res):
-        if not node:
-            return
-        if all (c is None for c in node.children):
-            res.append('->'.join(path+[str(node.val)]))
 
-        for child in node.children:
-            dfs (child,path + [str(node.val)],res)
-
-    if root:
-        dfs (root,[],res)
-    return res
+    def ternary_tree_paths(root: Node) -> list[str]:
+        res = []
+        def dfs (node,path,res):
+            if not node:
+                return
+            if all (c is None for c in node.children):
+                res.append('->'.join(path+[str(node.val)]))
+    
+            for child in node.children:
+                if child is not None:                 
+                    path.append (str(node.val))
+                    
+                    dfs (child,path,res)
+                    path.pop() # this removes from the path
+    
+        if root:
+            dfs (root,[],res)
+        return res
 
 # this function builds a tree from input; you don't have to modify it
 # learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
